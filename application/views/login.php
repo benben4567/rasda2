3e1c48ff4f0fac898623bd006440495fcd2ba200
benben4567 <benben4567@gmail.com>
2018-05-22 14:55:52 +0700

    dashboard admin

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Dinsos</title>
+    <title>Login Page</title>
 	<!-- BOOTSTRAP STYLES-->
     <link href="<?php echo base_url()?>assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FONTAWESOME STYLES-->
@@ -12,50 +12,40 @@
     <link href="<?php echo base_url()?>assets/css/custom.css" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
 </head>
 <body>
     <div class="container">
         <div class="row text-center ">
             <div class="col-md-12">
                 <br /><br />
-                <h2>DINSOS</h2>
                  <br />
             </div>
         </div>
-         <div class="row ">
-               
-                  <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                        <strong>   Enter Details To Login </strong>  
-                            </div>
-                            <div class="panel-body">
-                                <form role="form">
-                                       <br />
-                                     <div class="form-group input-group">
-                                            <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
-                                            <input type="text" class="form-control" placeholder="Your Username " />
-                                        </div>
-                                                                              <div class="form-group input-group">
-                                            <span class="input-group-addon"><i class="fa fa-lock"  ></i></span>
-                                            <input type="password" class="form-control"  placeholder="Your Password" />
-                                        </div>
-                                     
-                                     <a href="index.html" class="btn btn-primary ">Login Now</a>
-                                    <hr />
-                                    </form>
-                            </div>
-                           
-                        </div>
+        <div class="row ">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong>Login</strong>  
                     </div>
-                
-                
+                    <div class="panel-body">
+                        <form role="form">
+                            <br />
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
+                                <input type="text" class="form-control" placeholder="Your Username " />
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"  ></i></span>
+                                <input type="password" class="form-control"  placeholder="Your Password" />
+                            </div>
+                            <a href="index.html" class="btn btn-primary ">Login</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-
-     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
     <script src="<?php echo base_url()?>assets/js/jquery-1.10.2.js"></script>
       <!-- BOOTSTRAP SCRIPTS -->
@@ -72,7 +62,6 @@
 
   ga('create', 'UA-38955291-1', 'auto');
   ga('send', 'pageview');
-
 </script>
 </body>
 </html>
